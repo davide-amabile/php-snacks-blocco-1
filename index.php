540dc34d94478890fe359730c2337a7b01f4c793
Unknown <davideamabile81@gmail.com>
2020-10-05 16:11:38 +0200

    stampa partite

--- index.php
+++ index.php
@@ -1,15 +1,36 @@
 <?php
+
 // creare un array matches
-$matches[
+$matches = [
   // creare più array con proprietà nomi squadre e punteggio
-  "first_match" =>[
+  [
     "home" => "Armani Milano",
-    "away" => "Alma Trieste",
-    "score_home" => 93,
+    "away" => "DeLonghi Treviso",
+    "score_home" => 104,
+    "score_away" => 64,
+  ],
+
+  [
+    "home" => "Vanoli Cremona",
+    "away" => "Umana Venezia",
+    "score_home" => 66,
+    "score_away" => 83,
+  ],
+
+  [
+    "home" => "Dolomiti Trentino",
+    "away" => "reggio Emilia",
+    "score_home" => 81,
+    "score_away" => 87,
+  ],
+
+  [
+    "home" => "Fortitudo Bologna",
+    "away" => "Varese",
+    "score_home" => 83,
     "score_away" => 88,
   ],
 ];
-
 
 
 
@@ -21,7 +42,14 @@ $matches[
     <meta charset="utf-8">
     <title>snacks</title>
     <h1>SNACK 1</h1>
-    <span><?php echo $matches[0]["home"]; ?></span>
+    <p>
+      <?php
+      // creare ciclo per stampare tutti risultati
+       for ($i = 0; $i<=count($matches); $i++){
+        echo  $matches[$i]["home"]." - ".$matches[$i]["away"].":"." ".$matches[$i]["score_home"]." - ".$matches[$i]["score_away"];
+      }
+      ?>
+    </p>
   </head>
   <body>
 

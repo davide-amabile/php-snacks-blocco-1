af35a2b740f092399eeb0b7ea3d663b89d1544b6
Unknown <davideamabile81@gmail.com>
2020-10-05 18:02:44 +0200

    controllo. e numero su age

--- index2.php
+++ index2.php
@@ -20,7 +20,7 @@ $_GET["age"];
      <?php
        // fare i controlli sui GET
        $controll= "";
-       if(strlen($_GET["name"]) > 3 && strpos($_GET["mail"], "@") &&  ){
+       if(strlen($_GET["name"]) > 3 && strpos($_GET["mail"], "@") && strpos($_GET["mail"], ".") && is_numeric($_GET["age"])  ){
          $control= "Accesso riuscito";
          echo $control;
        }else{

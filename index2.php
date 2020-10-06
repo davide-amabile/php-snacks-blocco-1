9278aa7645f19e402626e5bd9f1ed920f0b1fa40
Unknown <davideamabile81@gmail.com>
2020-10-06 23:19:03 +0200

    controllo

--- index2.php
+++ index2.php
@@ -18,15 +18,15 @@ $_GET["age"];
     <h2>SNACK 2</h2>
     <p>
      <?php
-       // fare i controlli sui GET
-       $controll= "";
-       if(strlen($_GET["name"]) > 3 && strpos($_GET["mail"], "@") && strpos($_GET["mail"], ".") && is_numeric($_GET["age"])  ){
-         $control= "Accesso riuscito";
-         echo $control;
-       }else{
-         $control="Accesso negato";
-         echo $control;
-       }
+        // fare i controlli sui GET
+        $controll= "";
+        if(strlen($_GET["name"]) > 3 && strpos($_GET["mail"], "@") && strpos($_GET["mail"], ".") && is_numeric($_GET["age"])  ){
+          $control= "Accesso riuscito";
+          echo $control;
+        }else{
+          $control="Accesso negato";
+          echo $control;
+        }
      ?>
     </p>
   </body>
